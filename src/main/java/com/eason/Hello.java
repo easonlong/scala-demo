04d025c8866a8a692a1f457ed124c837a9fc730d
longyaokun <longyaokun@didichuxing.com>
2015-12-08 16:56:19 +0800

    scala hello world

---
+++ src/main/java/com/eason/Hello.java
@@ -0,0 +1,10 @@
+package com.eason;
+
+/**
+ * Created by longyaokun on 2015-12-7.
+ */
+public class Hello {
+    public String hello(){
+        return "Hello World";
+    }
+}
---
+++ src/main/scala/com/eason/scala/HelloWorld.scala
@@ -0,0 +1,14 @@
+package com.eason.scala;
+
+import com.eason.Hello
+
+/**
+  * Created by longyaokun on 2015-12-7.
+  */
+
+object HelloWorld{
+  def main(args: Array[String]) {
+    print(new Hello().hello())
+  }
+}
+
